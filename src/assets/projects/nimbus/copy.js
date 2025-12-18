7281917b388f1f753e9559916706afc595a56b30
Nathapong <101572359+nathapongn@users.noreply.github.com>
2025-12-18 01:43:05 +0000

    Copy.js

---
+++ src/assets/projects/nimbus/copy.js
@@ -0,0 +1,16 @@
+const copy = {
+    intro: {
+        title: "Nimbus",
+        description: "Streamlines personal finance management with automation.",
+        tools: {
+            title: "Tools",
+            description: ["Microsoft Excel", "Adobe Premiere Pro", "Adobe Photoshop"]
+        },
+        roles: {
+            title: "Roles",
+            description: "Individual Project"
+        }
+    }
+}
+
+export default copy;
--- src/pages/projects/nimbus/sections/Hero.jsx
+++ src/pages/projects/nimbus/sections/Hero.jsx
@@ -5,6 +5,7 @@ import Divider from "../../../../components/primitives/Divider/Divider.jsx";
 
 // Assets
 import assets from '../../../../assets/projects/nimbus/index.js';
+import copy from '../../../../assets/projects/nimbus/copy.js';
 
 export default function Hero() {
     return (
@@ -18,31 +19,31 @@ export default function Hero() {
             />
             <ColumnLayout>
                 <Copy
-                    title="Nimbus"
+                    title={copy.intro.title}
                     titleSize="text-xxl"
-                    description="Streamlines personal finance management with automation."
+                    description={copy.intro.description}
                     descriptionSize="text-m"
                     cols="6"
                     textAlign="left"
                 />
                 <Copy
-                    title="Tools"
+                    title={copy.intro.tools.title}
                     titleSize="text-m strong"
                     description={
                     <ul className="list-plain">
-                        <li>Figma</li>
-                        <li>Microsoft Excel</li>
-                        <li>Adobe Premiere</li>
-                        <li>Adobe Photoshop</li>
+                        <li>{copy.intro.tools.description[0]}</li>
+                        <li>{copy.intro.tools.description[1]}</li>
+                        <li>{copy.intro.tools.description[2]}</li>
+                        <li>{copy.intro.tools.description[3]}</li>
                     </ul>}
                     descriptionSize="text-m"
                     cols="3"
                     textAlign="left"
                 />
                 <Copy 
-                    title="Role"
+                    title={copy.intro.roles.title}
                     titleSize="text-m strong"
-                    description="Individual Project"
+                    description={copy.intro.roles.description}
                     descriptionSize="text-m"
                     cols="3"
                     textAlign="left"
